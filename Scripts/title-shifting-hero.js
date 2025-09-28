274c855b4b73c78f3caae4e779c22d46f0bc5876
SergeantKPR <awpertunitytaken@gmail.com>
2025-09-28 12:41:40 -0400

    Updated index + guides and added images and scripts

---
+++ Scripts/title-shifting-hero.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll',() => 
+    {
+        const hero = document.querySelector('.hero');
+        const h1 = hero.querySelector('h1');
+
+        const rect = hero.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        const progress = 1 - (rect.bottom / windowHeight);
+
+        const shift = progress * 150; // Adjust multiplier for more/less shift
+        h1.style.setProperty('--title-shift', `${shift}px`);
+    });
